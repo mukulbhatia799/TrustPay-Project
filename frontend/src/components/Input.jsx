43c4d2036c3bc79f8d0afd22f5ba7563ad4eb2f4
Mukul Bhatia <mukulbhatia189@gmail.com>
2024-06-22 00:55:58 +0530

    added email exists warning

--- frontend/src/components/Input.jsx
+++ frontend/src/components/Input.jsx
@@ -6,6 +6,7 @@ export function Input(props) {
             <div className="flex flex-col">
                 <div className="font-bold text-lg">{props.label}</div>
                 <input onChange={props.onChange} className="border border-zinc-400 rounded px-3 h-10 min-w-full hover:border-black" type={props.inputType} placeholder={props.placeholder} />
+                <div className="text-red-600">{props.emailWarning}</div>
             </div>
         </>
     )
