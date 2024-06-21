2b720b27f1d9748b5f9d5986ba83cf0cd973a5e3
Mukul Bhatia <mukulbhatia189@gmail.com>
2024-06-22 00:57:05 +0530

    removed console warnings.

--- frontend/src/components/WarningAtEnd.jsx
+++ frontend/src/components/WarningAtEnd.jsx
@@ -3,10 +3,10 @@ import { Navigate, useNavigate } from 'react-router-dom'
 export function WarningAtEnd(props) {
     const navigate = useNavigate();
     return (
-        <p class="text-sm font-light text-black">
+        <p className="text-sm font-light text-black">
             {props.text} <button onClick={() => {
                 navigate(props.to);
-            }} class="font-medium text-primary-800 hover:underline">{props.linkText}</button>
+            }} className="font-medium text-primary-800 hover:underline">{props.linkText}</button>
         </p>
     )
 }
